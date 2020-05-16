9f62c7fb948c0a99ab659e7a18a2b7a219c513b2
Rohan <rohan17.rp@gmail.com>
2020-05-16 15:49:02 +0530

    Heroku Launching

--- Idle/app.js
+++ Idle/app.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,6 +13,6 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log('listening on port: ' + port);
 });
